101baeffe00e9d20cd2a209168c34147fffec619
Aamir <aamir104512@gmail.com>
2022-06-04 19:16:11 +0500

    Workout and diet module navigation in bottom navigator

---
+++ src/Navigation/DietNavigation.js
@@ -0,0 +1,38 @@
+import {View, Text} from 'react-native';
+import React from 'react';
+
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import Colors from '../colors/Colors';
+import CustomHeader from '../components/CustomHeader';
+const Stack = createNativeStackNavigator();
+import DietPlans from '../Screens/User/DietModule/DietPlans';
+import RecipeDetails from '../Screens/User/DietModule/RecipeDetails';
+const DietNavigation = props => {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen
+        name="Diet Plans"
+        component={DietPlans}
+        options={{
+          headerTitle: () => (
+            <CustomHeader title="Diet Plans" drawer={props.navigation} />
+          ),
+          headerStyle: {backgroundColor: Colors.primary},
+          headerBackVisible: false,
+        }}
+      />
+      <Stack.Screen
+        name="Recipe"
+        component={RecipeDetails}
+        options={{
+          headerTitle: () => (
+            <CustomHeader title="Recipe Details" drawer={props.navigation} />
+          ),
+          headerStyle: {backgroundColor: Colors.primary},
+          headerBackVisible: false,
+        }}
+      />
+    </Stack.Navigator>
+  );
+};
+export default DietNavigation;
